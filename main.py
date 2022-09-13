154bd2f1e7f88421485b5801688c890d51c5fd86
dnm <normurodovwork@gmail.com>
2022-09-13 20:30:56 +0500

    __mull___

--- main.py
+++ main.py
@@ -18,17 +18,63 @@
 # c = A('Yakob', 20)
 # print(a+b-c-a)
 #
-class B:
-    def __init__(self, name):
-        self.name = name
+# class B:
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def __setattr__(self, attr, value):
+#         if attr == 'name':
+#             self.__dict__[attr] = value
+#         else:
+#             raise AttributeError
+#
+#
+# d = B('yoyo')
+# d.name = 'Koko'
+# print(d.name)
+# class C:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def __setattr__(self, key, value):
+#         self.__dict__['name'] = key
+#         self.__dict__['age'] = value
+#
+#     def __str__(self):
+#         return f'{self.name}: {self.age}'
+#
+#
+# c = C('pep', 56)
+# c.name = 'rop'
+# c.weight = 45
+# print(c)
 
-    def __setattr__(self, attr, value):
-        if attr == 'name':
-            self.__dict__[attr] = value
-        else:
-            raise AttributeError
+class Bolb:
+    def __init__(self, name, w, v):
+        self._name = name
+        self._w = w
+        self._v = v
+
+    def get_name(self):
+        return self._name
+
+    def get_v(self):
+        return self._v
+
+    def get_w(self):
+        return self._w
+
+    def __str__(self):
+        return f'{self._name} {self._w} {self._v}'
+
+    def __add__(self, other):
+        self._name = self._name + ' & ' + other.get_name()
+        self._v = self._v * other.get_v()
+        self._w = self._w * other.get_w()
+        return self
 
 
-d = B('yoyo')
-d.name = 'Koko'
-print(d.name)
+c = Bolb('Folp', 34, 45)
+d = Bolb('Golp', 44, 65)
+print(c+d)
